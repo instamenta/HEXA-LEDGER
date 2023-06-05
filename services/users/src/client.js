68229d17aaed53fa494f8619672e8ebb4519b602
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-06-05 16:32:57 +0300

    master: formated and optimizated users/service

--- services/users/src/client.js
+++ services/users/src/client.js
@@ -1,28 +1,25 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const { User } = require('./generated/users_pb');
-
-const packageDefinition = protoLoader.loadSync('./src/protos/users.proto', {
+const grpc = require('@grpc/grpc-js')
+    , protoLoader = require('@grpc/proto-loader')
+    , {User} = require('./generated/users_pb')
+;
+const packageDefinition = protoLoader.loadSync('./protos/users.proto', {
     keepCase: true,
     longs: String,
     enums: String,
     defaults: true,
     oneofs: true,
-});
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const userProto = protoDescriptor.user;
+})
+    , protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+    , userProto = protoDescriptor.user
+    , CLIENT = new userProto.UserService( 'localhost:50051', grpc.credentials.createInsecure() )
+;
 
-const client = new userProto.UserService(
-    'localhost:50051',
-    grpc.credentials.createInsecure()
-);
-
-// Perform login request
+/* Perform login request */
 const loginRequest = new User();
 loginRequest.setEmail('test@example.com');
 loginRequest.setPassword('password');
 
-client.login(loginRequest, (error, response) => {
+CLIENT.login(loginRequest, (error, response) => {
     if (error) {
         console.error('Login error:', error);
         return;
@@ -31,13 +28,13 @@ client.login(loginRequest, (error, response) => {
     console.log('Login response:', response);
 });
 
-// Perform register request
+/* Perform register request */
 const registerRequest = new User();
 registerRequest.setEmail('test@example.com');
 registerRequest.setNickname('John Doe');
 registerRequest.setPassword('password');
 
-client.register(registerRequest, (ERROR, response) => {
+CLIENT.register(registerRequest, (ERROR, response) => {
     if (ERROR) {
         console.error('Register error:', ERROR);
         return;
--- services/users/src/server.js
+++ services/users/src/server.js
@@ -1,33 +1,32 @@
-const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader');
-const userService = require('./services/userService');
-
+const grpc = require('@grpc/grpc-js')
+    , protoLoader = require('@grpc/proto-loader')
+    , userService = require('./services/userService')
+;
 const packageDefinition = protoLoader.loadSync('./src/protos/users.proto', {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-});
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const userProto = protoDescriptor.user;
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true,
+    })
+    , protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+    , userProto = protoDescriptor.user
+;
 
-function main() {
-    const server = new grpc.Server();
-    server.addService(userProto.UserService.service, {
+(function main() {
+    const SERVER = new grpc.Server();
+    SERVER.addService(userProto.UserService.service, {
         login: userService.login,
         register: userService.register,
     });
 
     const PORT = process.env.PORT || 50051;
-    server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (ERROR, PORT) => {
+    SERVER.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (ERROR, PORT) => {
         if (ERROR) {
             console.error(`Failed to bind server on PORT: ${PORT}: `, ERROR);
             process.exit(1);
         }
         console.log(`Server is running on port: ${PORT}`);
-        server.start();
+        SERVER.start();
     });
-}
-
-main();
+})()

e9411b794f1caaa538e4b585079d8c3ff9bcbb7e
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-07-15 14:14:44 +0300

    master: too many changes to count

--- services/post-remote/src/utility/token-tools.ts
+++ services/post-remote/src/utility/token-tools.ts
@@ -5,45 +5,46 @@ const TOKEN_SECRET = process.env.TOKEN_SECRET || 'SECRET';
 
 class TokenTools {
 
-	/**
-	 * @param u
-	 * @returns
-	 */
-	public static GENERATE_TOKEN(u: IUser): Promise<string> {
-		const PAYLOAD = {
-			username: u.username,
-			email: u.email,
-			_id: u._id,
-			picture: u.picture,
-		};
-		return new Promise((resolve, reject) => {
-			sign(PAYLOAD, 'SECRET', {expiresIn: '60 days'}, (error, token) => error
-				? reject(error)
-				: resolve(token!)
-			);
-		});
-	}
+    /**
+     * @param u
+     * @returns
+     */
+    public static GENERATE_TOKEN(u: IUser): Promise<string> {
+        const PAYLOAD = {
+            username: u.username,
+            email: u.email,
+            _id: u._id,
+            picture: u.picture,
+        };
+        return new Promise((resolve, reject) => {
+            sign(PAYLOAD, 'SECRET', {expiresIn: '60 days'},
+                (error, token) => error
+                    ? reject(error)
+                    : resolve(token!)
+            );
+        });
+    }
 
-	/**
-	 * @param token
-	 * @returns
-	 */
-	public static DECODE_TOKEN(token: string): Promise<string> {
-		return new Promise((resolve, reject) => {
-			verify(token, 'SECRET', (error, decoded) => error
-				? reject(error)
-				: resolve(<string>decoded)
-			);
-		});
-	}
+    /**
+     * @param token
+     * @returns
+     */
+    public static DECODE_TOKEN(token: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            verify(token, 'SECRET', (error, decoded) => error
+                ? reject(error)
+                : resolve(<string>decoded)
+            );
+        });
+    }
 
-	/**
-	 * @param token
-	 * @returns
-	 */
-	public static VERIFY_TOKEN(token: string): string {
-		return <string>verify(token, TOKEN_SECRET);
-	}
+    /**
+     * @param token
+     * @returns
+     */
+    public static VERIFY_TOKEN(token: string): string {
+        return <string>verify(token, TOKEN_SECRET);
+    }
 }
 
 export default TokenTools;

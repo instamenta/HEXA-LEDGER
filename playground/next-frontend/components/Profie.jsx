f84473d5e94df302e73d455a0493897d162766f9
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-07-18 23:54:54 +0300

    master: removed, moved & added template for new prisma service for better abstraction

--- playground/next-frontend/components/Profie.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const Profie = () => {
-    return (
-        <div>Profie</div>
-    )
-}
-export default Profie
--- playground/next-frontend/components/PromptCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const PromptCard = () => {
-    return (
-        <div>PromptCard</div>
-    )
-}
-export default PromptCard

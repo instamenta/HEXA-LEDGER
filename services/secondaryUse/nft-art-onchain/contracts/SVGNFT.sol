a14904059574587acbff63118d1e3626eb474d41
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-08-20 22:28:53 +0300

    master: minor changes done to the small web3 project

--- services/secondaryUse/nft-art-onchain/contracts/SVGNFT.sol
+++ services/secondaryUse/nft-art-onchain/contracts/SVGNFT.sol
@@ -7,5 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract SVGNFT is ERC721URIStorage {
     constructor() ERC721("SVG NFT", "svgNFT") {}
 
-    function create(string memory svg) public {}
+    function create(string memory svg) public {
+        
+    }
 }

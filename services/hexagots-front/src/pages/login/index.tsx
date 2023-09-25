514793d860baafd3047e8007610da4561e5121e5
Jan Milenkov <jan.milenkov@globalinfra.tech>
2023-09-26 00:11:24 +0300

    changed nextjs frontend to app router

--- services/hexagots-front/src/pages/login/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-   return(
-      <div className="flex align-middle justify-center  w-max">
-      <SignIn />
-   </div>
-   );
-}
--- services/hexagots-front/src/pages/register/index.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-   return (
-      <div className="flex align-middle w-max">
-         <SignUp />
-      </div>
-   )
-}
